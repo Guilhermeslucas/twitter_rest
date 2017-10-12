5d631d63e660c4b01c8bfeddb59f9751d1769af5
Guilhermeslucas <guilherme.slucas@gmail.com>
2017-10-11 22:31:14 -0300

    fixing

--- twitter_api/views.py
+++ twitter_api/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from django.http import HttpResponse
 from rest_framework import status
 from . import twitter_methods
 from rest_framework.response import Response
@@ -9,5 +8,4 @@ class Tweets(APIView):
     def get(self, request, *args, **kwargs):
         search_arg = kwargs['search_pattern']
         result = twitter_methods.get_search(search_arg, 200)
-        response = Response({'data':result})
-        return response
+        return Response({'data': [for r._json in result]})
