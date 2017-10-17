9fd29371cec5edfbe9c479d3de62c2330e923b13
Guilhermeslucas <guilherme.slucas@gmail.com>
2017-10-16 22:47:39 -0200

    adding new branch with all avaiable information

--- twitter_api/views.py
+++ twitter_api/views.py
@@ -7,21 +7,5 @@ from rest_framework.response import Response
 class Tweets(APIView):
     def get(self, request, *args, **kwargs):
         search_arg = kwargs['search_pattern']
-        result = twitter_methods.get_search(search_arg, 1)
-        results = []
-        for r in result:
-            new_result = {}
-            aux_result = r._json
-            new_result['text'] = r.text
-            new_result['id'] = r.id
-            new_result['created_at'] = r.created_at
-            new_result['in_reply_to_status_id'] = r.in_reply_to_status_id
-            new_result['in_reply_to_screen_name'] = r.in_reply_to_screen_name
-            new_result['retweet_count'] = r.retweet_count
-            new_result['favorite_count'] = r.favorite_count
-            new_result['coordinates'] = r.coordinates
-            new_result['user_name'] = r.user.screen_name
-            new_result['user_followers'] = r.user.followers_count
-            new_result['user_following'] = r.user.friends_count
-            results.append(aux_result)
-        return Response({'data': results})
+        result = twitter_methods.get_search(search_arg, 20)
+        return Response({'data': r._json for r in result})
